02eec7d701694144a4f61cb338aba9dfd169faa4
Albert Young <ayoung01@college.harvard.edu>
2013-04-21 15:45:48 -0400

    Implemented KK

--- NumberPartition.java
+++ NumberPartition.java
@@ -6,11 +6,24 @@ public class NumberPartition {
   String[] test_args = {"hellozz"};
 	public static void main(String[] args) {
 		System.out.println("Hello World");
-    test(test_args);
 	}
 
-  public static int test(String[] test_args){
-    System.out.println(test_args[0]);
-    return 2;
+  public static long KarmarkarKarp(Long[] arr) {
+    MaxHeap<Long> heap = new MaxHeap<Long>(arr, arr.length, arr.length);
+    if (arr.length < 1) {
+      System.out.println("Incorrect input size");
+      return -1;
+    }
+    // while max heap has more than 1 element
+    // remove top two elements from max heap and take the difference
+    while (heap.heapsize() > 1) {
+      long a = heap.removemax().longValue();
+      long b = heap.removemax().longValue();
+      Long difference = new Long (a - b);
+      // reinsert the difference into the heap
+      heap.insert(difference);
+    }
+    // when the heap contains 1 element, return
+    return heap.removemax();
   }
 }
