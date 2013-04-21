d3ff77423577daf9d39ac094880b49caa5e9ca31
Peregrine Badger <pbadger@college.harvard.edu>
2013-04-21 14:59:54 -0400

    test push

--- NumberPartition.java
+++ NumberPartition.java
@@ -2,7 +2,15 @@ public class NumberPartition {
 	public NumberPartition() {
 
 	}
+
+  String[] test_args = {"hellozz"};
 	public static void main(String[] args) {
 		System.out.println("Hello World");
+    test(test_args);
 	}
+
+  public static int test(String[] test_args){
+    System.out.println(test_args[0]);
+    return 2;
+  }
 }
