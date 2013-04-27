549a4989e54fcc741ab94e2f2ebdc9e2285ba127
Peregrine Badger <pbadger@college.harvard.edu>
2013-04-27 15:04:54 -0400

    doesnt work, pre-merge

--- PrePartition.java
+++ PrePartition.java
@@ -1,3 +1,4 @@
+import java.lang.Math;
 import java.util.Random;
 import java.util.Collections;
 import java.util.Arrays;
@@ -60,9 +61,10 @@ public class PrePartition {
     // Choose two random indices i and j from [1, n] with p_i != j and set p_i to j IF RESIDUE IS BETTER
     int[] p_copy = p.clone();
     int j;
-    int i = (generator.nextInt() % a_prime.length);
+    int i = generator.nextInt(a_prime.length);  
+    // System.out.println(i);
     do {
-      j = (generator.nextInt() % a_prime.length);
+      j = generator.nextInt(a_prime.length);
     } while(j != p_copy[i]);
     p_copy[i] = j;
 
@@ -73,6 +75,44 @@ public class PrePartition {
     if (new_residue < residue) {
       p = p_copy;
       a_prime = prePartition(a_orig);
+      System.out.println("BETTER ");
     }
+  }
+
+  public void get_annealed_neighbor(double temp){
+    // Choose two random indices i and j from [1, n] with p_i != j and set p_i to j IF RESIDUE IS BETTER
+    int[] p_copy = p.clone();
+    int j;
+    int i = generator.nextInt(a_prime.length);
+    do {
+      j = generator.nextInt(a_prime.length);
+    } while(j != p_copy[i]);
+    p_copy[i] = j;
+
+    KarmarkarKarp kk = new KarmarkarKarp(a_prime, 0);
+    Long new_residue = kk.residue();
+
+    temp = get_temperature(temp);
+    System.out.println("temp: " + temp);
+    System.out.println("new_residue: " + new_residue);
+    System.out.println("old residue: " + residue);
+    Double exponent = ((double)new_residue - (double)residue)/(temp);
+    System.out.println(exponent);
+    exponent = Math.floor(exponent);
+    // System.out.println(exponent);
+    Double probability = Math.pow(2.18, exponent);
+    Double choose_neighbor = Math.abs(generator.nextInt() % probability);
+    // Update if a better neighbor has been found
+    if (new_residue < residue) {
+      p = p_copy;
+      a_prime = prePartition(a_orig);
+    }
+    else if(choose_neighbor == 1){
+      System.out.println("bad jump! "+temp);
+    }  
+  }
+
+  public double get_temperature(double ratio){
+    return ratio*10;
   }
 }
